611cf0c7657bcafb641589a3e4a272e8079d626c
Élcio Mateus Fernandes <elciomateusfernandes1@gmail.com>
2024-11-04 06:40:06 -0300

    add log files for tasks

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -5,12 +5,24 @@ def task(func):
     async def wrapper(*args, **kwargs):
         hashlog = hashlib.sha256(f"{func.__module__.removeprefix('tasks.')}-{datetime.datetime.now()}".encode()).hexdigest()[:32]
 
-        print(f"{datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - INFO - {hashlog} - Running {func.__module__.removeprefix('tasks.')}...")
-        if asyncio.iscoroutinefunction(func):
-            r = await func(*args, **kwargs)
-        else:
-            r = func(*args, **kwargs)
-        print(f"{datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - INFO - {hashlog} - Result from {func.__module__.removeprefix('tasks.')}: {r}.")
+        if not os.path.exists("tasks/log"):
+            os.mkdir("tasks/log")
+
+        if not os.path.exists(f"tasks/log/{func.__module__.removeprefix('tasks.')}"):
+            os.mkdir(f"tasks/log/{func.__module__.removeprefix('tasks.')}")
+
+        with open(f"tasks/log/{func.__module__.removeprefix('tasks.')}/{datetime.datetime.today().strftime('%Y%m%d%H%M%S')}.log", 'a', -1, 'utf-8') as f:
+
+            f.write(f"{datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - INFO - {hashlog} - Running {func.__module__.removeprefix('tasks.')}...\n")
+
+            f.write(f"{datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - INFO - {hashlog} - Args: {args}. Kwargs: {kwargs}.\n")
+
+            if asyncio.iscoroutinefunction(func):
+                r = await func(*args, **kwargs)
+            else:
+                r = func(*args, **kwargs)
+
+            f.write(f"{datetime.datetime.now().strftime('%d-%m-%Y %H:%M:%S')} - INFO - {hashlog} - Result from {func.__module__.removeprefix('tasks.')}: {r}.\n")
         return r
     return wrapper
 
